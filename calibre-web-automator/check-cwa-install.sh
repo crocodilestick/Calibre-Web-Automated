b1383e1560ebaceadcaf72b489da35cefcfa9fbc
CrocodileStick <105450872+crocodilestick@users.noreply.github.com>
2024-06-25 13:41:53 +0000

    Fixed colour output in terminal prompts

--- calibre-web-automator/check-cwa-install.sh
+++ calibre-web-automator/check-cwa-install.sh
@@ -9,26 +9,26 @@ echo "==== Calibre-Web Automator -- Status of Monitoring Services ===="
 echo ""
 
 if s6-rc -a list | grep -q 'calibre-scan'; then
-    echo "- Calibre-scan ${GREEN}is running${NC}"
+    echo -e "- Calibre-scan ${GREEN}is running${NC}"
     cs=true
 else
-    echo "- Calibre-scan ${RED}is not running${NC}"
+    echo -e "- Calibre-scan ${RED}is not running${NC}"
     cs=false
 fi
 
 
 if s6-rc -a list | grep -q 'books-to-process-scan'; then
-    echo "- Books-to-process-scan ${GREEN}is running${NC}"
+    echo -e "- Books-to-process-scan ${GREEN}is running${NC}"
     bs=true
 else
-    echo "- Books-to-process-scan ${RED}is not running${NC}"
+    echo -e "- Books-to-process-scan ${RED}is not running${NC}"
     bs=false
 fi
 
 echo ""
 
 if $cs && $bs; then
-    echo "Calibre-Web-Automater was ${GREEN}sucsessfully installed ${NC}and ${GREEN}is running properly!${NC}"
+    echo -e "Calibre-Web-Automater was ${GREEN}sucsessfully installed ${NC}and ${GREEN}is running properly!${NC}"
 else
-    echo "Calibre-Web-Automater was ${RED}not installed sucsessfully${NC}, please check the logs for more information."
+    echo -e "Calibre-Web-Automater was ${RED}not installed sucsessfully${NC}, please check the logs for more information."
 fi
--- calibre-web-automator/setup-cwa.sh
+++ calibre-web-automator/setup-cwa.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Script to automatically enable the automatic importing of epubs from the 'to_calibre' import folder upon container restart
 # For help with S6 commands ect.: https://wiki.artixlinux.org/Main/S6
 
@@ -31,7 +34,7 @@ cp "$SCRIPT_DIR/check-cwa-install.sh" /etc/calibre-web-automator/check-cwa-insta
 echo "" | cat >> ~/.bashrc
 echo "# Calibre-Web Automator Aliases" | cat >> ~/.bashrc
 echo "alias cwa-check='bash /etc/calibre-web-automator/check-cwa-install.sh'" | cat >> ~/.bashrc
-echo "alias cwa-change-dirs='nano /etc/calibre-web-automater/dirs.json'" | cat >> ~/.bashrc
+echo "alias cwa-change-dirs='nano /etc/calibre-web-automator/dirs.json'" | cat >> ~/.bashrc
 source ~/.bashrc
 
 # Setup inotify to watch for changes in the 'to_calibre' folder
@@ -50,10 +53,10 @@ touch /etc/s6-overlay/s6-rc.d/user/contents.d/books-to-process-scan
 
 # Setup completion notification
 echo ""
-echo "SUCSESS: Calibre-Web-Automator Setup Complete!"
+echo -e "${GREEN}SUCSESS${NC}: Calibre-Web-Automator Setup Complete!"
 echo ""
 echo " - Please restart the container so the changes will take effect."
 echo " - Do so by typing 'exit', presing enter, then running the docker command:"
-echo " - 'docker restart <name-of-your-calibre-web-container'"
+echo -e "    -- '${GREEN}docker restart <name-of-your-calibre-web-container${NC}'"
 echo ""
-echo "To check if CWA is running properly following the restart, use the command 'cwa-check' in the container's terminal."
+echo -e "To check if CWA is running properly following the restart, use the\ncommand '${GREEN}cwa-check${NC}' in the container's terminal."

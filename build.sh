471a894aeba0cf6d722e3977363634c442476ccb
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2024-09-29 01:05:18 -0700

    don't overwrite docker-compose.yml in build/ if already exists

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build --tag cwa-dev .
 mkdir -p build
-cp docker-compose.yml.dev build/docker-compose.yml
+cp -n docker-compose.yml.dev build/docker-compose.yml
 cd build/
 docker compose up -d
 

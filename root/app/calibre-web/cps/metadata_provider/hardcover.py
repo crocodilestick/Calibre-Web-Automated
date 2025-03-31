397dffa02286c2a119e5e78aee0ff29dd3b367a7
demitrix <demitrix@gmail.com>
2025-03-31 16:53:59 +0000

    Add Global Hardcover Token

--- root/app/calibre-web/cps/metadata_provider/hardcover.py
+++ root/app/calibre-web/cps/metadata_provider/hardcover.py
@@ -20,7 +20,7 @@
 from typing import Dict, List, Optional
 
 import requests
-from cps import logger
+from cps import logger, config
 from cps.services.Metadata import MetaRecord, MetaSourceInfo, Metadata
 from cps.isoLanguages import get_language_name
 from ..cw_login import current_user
@@ -100,7 +100,7 @@ class Hardcover(Metadata):
         val = list()
         if self.active:
             try:
-                token = current_user.hardcover_token or getenv("HARDCOVER_TOKEN")
+                token = current_user.hardcover_token or config.config_hardcover_token or getenv("HARDCOVER_TOKEN")
                 if not token:
                     self.set_status(False)
                     raise Exception("Hardcover token not set for user, and no global token provided.")

d4569a7613eddd128b2517ebeb7baedff2f67b27
crocodilestick <105450872+crocodilestick@users.noreply.github.com>
2025-08-19 16:07:03 +0200

    Fixed spdx headers

--- cps/utils/filename_sanitizer.py
+++ cps/utils/filename_sanitizer.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# Calibre-Web Automated – fork of Calibre-Web
+# Copyright (C) 2018-2025 Calibre-Web contributors
+# Copyright (C) 2024-2025 Calibre-Web Automated contributors
+# SPDX-License-Identifier: GPL-3.0-or-later
+# See CONTRIBUTORS for full list of authors.
+
 # Shared filename sanitizer to mirror Calibre‑Web's behavior
 # SPDX-License-Identifier: GPL-3.0-or-later
 

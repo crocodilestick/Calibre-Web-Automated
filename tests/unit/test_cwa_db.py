e6b0c38cb5f7897474c24a557eab285198ba7c6b
crocodilestick <105450872+crocodilestick@users.noreply.github.com>
2026-01-29 23:56:43 +0100

    Update copyright years in all relevant files to 2026 for Calibre-Web contributors and Calibre-Web Automated contributors

--- tests/unit/test_cwa_db.py
+++ tests/unit/test_cwa_db.py
@@ -1,6 +1,6 @@
 # Calibre-Web Automated – fork of Calibre-Web
-# Copyright (C) 2018-2025 Calibre-Web contributors
-# Copyright (C) 2024-2025 Calibre-Web Automated contributors
+# Copyright (C) 2018-2026 Calibre-Web contributors
+# Copyright (C) 2024-2026 Calibre-Web Automated contributors
 # SPDX-License-Identifier: GPL-3.0-or-later
 # See CONTRIBUTORS for full list of authors.
 
--- tests/unit/test_duplicates_timezone.py
+++ tests/unit/test_duplicates_timezone.py
@@ -1,7 +1,8 @@
 # Calibre-Web Automated – fork of Calibre-Web
-# Copyright (C) 2018-2025 Calibre-Web contributors
-# Copyright (C) 2024-2025 Calibre-Web Automated contributors
+# Copyright (C) 2018-2026 Calibre-Web contributors
+# Copyright (C) 2024-2026 Calibre-Web Automated contributors
 # SPDX-License-Identifier: GPL-3.0-or-later
+# See CONTRIBUTORS for full list of authors.
 
 from datetime import datetime, timezone
 from types import SimpleNamespace, ModuleType
